ddb6aa5d5b575f87806df2f32d54384eb2957dec
Joe Moore <protean@gmail.com>
2017-12-31 20:51:12 -0600

    Functioning if ugly form for lineup creation

--- app/controllers/lineups_controller.rb
+++ app/controllers/lineups_controller.rb
@@ -4,6 +4,19 @@ class LineupsController < ApplicationController
   def index
   end
 
+  def new
+    @lineup = Lineup.new
+  end
+
+  def create
+    @lineup = Lineup.new(lineup_params)
+    if @lineup.save
+      redirect_to lineup_path(@lineup)
+    else
+      render 'new'
+    end
+end
+
   def show
     pdf = CommunionPdf.new(@lineup)
     send_data pdf.render,
@@ -16,5 +29,27 @@ class LineupsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_lineup
       @lineup = Lineup.find(params[:id])
+    end
+
+    def lineup_params
+      params.require(:lineup).permit( :service_date,
+                                      :position_1,
+                                      :position_2,
+                                      :position_3,
+                                      :position_4,
+                                      :position_5,
+                                      :position_6,
+                                      :position_7,
+                                      :position_8,
+                                      :position_9,
+                                      :position_10,
+                                      :position_11,
+                                      :position_12,
+                                      :position_13,
+                                      :position_14,
+                                      :position_15,
+                                      :position_16,
+                                      :position_17,
+                                      :position_18)
     end
 end
